8d09477d457a8d0bc3018cee1cec9a7ad882a3ea
Srinath <srinathrajath0@gmail.com>
2022-07-12 16:04:44 +0530

    Script Commit

--- three.sh
+++ three.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-git init
 git add .
-git commit -m "[message]"
-git branch -M master
-git remote add origin "https://github.com/srinathrajath/project3.git"
-git push -u origin master
+git commit -m "Script Commit"
+git push -u origin main
